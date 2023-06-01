184e05b8d2bbb778fe38aa402903089369427300
Usmanksri <usmankasuri123@gmail.com>
2023-06-01 13:04:30 +0500

    manage th js code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,9 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "trix"
-import "@rails/actiontext"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "trix";
+import "@rails/actiontext";
 
-
-
-//console.log("javascript tested");
 $(document).ready(function() {
-  // Test code here
-  //console.log("jQuery is working!");
-
   $('input[type="radio"][name^="status_"]').change(function() {
     var taskID = $(this).data('taskId'); // Updated to use 'taskId'
     var status = $(this).val();
@@ -36,23 +29,36 @@ $(document).ready(function() {
     });
   });
 
-    // Pagination with scrolling
-    //debugger
-    $(window).on('scroll', function() {
-      var url = $('.pagination .next_page a').attr('href');
-      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-        $('.pagination').text('Loading more projects...');
-        $.getScript(url);
-      }
-    });
+  function append_listing(elementId) {
+    var next_page = $('#' + elementId).data('next-page');
+    if (next_page) {
+      var visitedUrl = undefined;
+      $(window).on('scroll', function() {
+        var url = $('.pagination a.next_page').attr('href');
+        if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 20) {
+          console.log('inside if scrolling');
+          console.log("url", url);
+          if (visitedUrl !== url) {
+            $.getScript(url);
+            visitedUrl = url;
+          }
+        }
+      });
+    }
+  }
+  append_listing('next_projects'); // Append projects
+  append_listing('next_tasks'); // Append tasks
 
 
+});
+
+
+
+// will be used later for showin and hiding of flash messages
   // // Show the flash notice container with a fade-in effect
   // $('.flash-notice-container, .flash-alert-container').fadeIn('slow');
-  
   // // Hide the flash notice containers after a certain time (e.g., 5 seconds)
   // setTimeout(function() {
   //   $('.flash-notice-container, .flash-alert-container').fadeOut('slow');
   // }, 5000);
-});
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -11,4 +11,7 @@ class Project < ApplicationRecord
     validates :name, presence: true
     validates :description, presence: true
 
+    self.per_page = 3
+
+
 end
